1bd078e8de0a3c2bef9ca3a6fd5bab7d719687d4
johnnie71 <johnnie.agonz71@gmail.com>
2022-12-30 17:07:21 -0500

    added camera perspective

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "red" });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+// Sizes
+const sizes = {
+	width: 800,
+	height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+scene.add(camera);
