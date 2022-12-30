78a38848d121c4fdf3f612a771b9dd3efe1a3e1d
johnnie71 <johnnie.agonz71@gmail.com>
2022-12-30 17:04:03 -0500

    created a scene and a red cube

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("HELLO Three JS");
+// Creating the scene
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
